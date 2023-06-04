82d0e1eaee79f13188d601e481c4a6cf107eb3e9
MonkeSpexz <135538171+MonkeSpexz@users.noreply.github.com>
2023-06-04 12:45:22 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,2 @@
 -- Redline loader
-loadstring(game:HttpGet('https://raw.githubusercontent.com/topitbopit/Redline/main/beta.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/MonkeSpexz/Redline/main/beta.lua'))()
